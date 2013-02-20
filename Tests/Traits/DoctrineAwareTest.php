e3a28633f7c31e7c17288c7d9115163a5fe80def
Pierre Minnieur <pm@pierre-minnieur.de>
2013-02-20 09:30:34 +0100

    fixed doctrine manager registry interface mock

--- Tests/Traits/DoctrineAwareTest.php
+++ Tests/Traits/DoctrineAwareTest.php
@@ -18,7 +18,7 @@ class DoctrineAwareTest extends \PHPUnit_Framework_TestCase
     public function testTrait()
     {
         $trait = new TestDoctrineAware();
-        $trait->setDoctrine($doctrine = $this->getMock('Symfony\\Bridge\\Doctrine\\RegistryInterface'));
+        $trait->setDoctrine($doctrine = $this->getMock('Doctrine\\Common\\Persistence\\ManagerRegistry'));
         $this->assertSame($doctrine, $trait->getDoctrine());
     }
 }
